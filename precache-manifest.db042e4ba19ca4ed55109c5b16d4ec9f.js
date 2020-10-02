97b887c4003597b1f1a94eb1481587ecaa8c6bab
Daniel <danisan9490@gmail.com>
2020-10-02 13:00:29 +0100

    Updates

---
+++ precache-manifest.db042e4ba19ca4ed55109c5b16d4ec9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf69608cca4a510f07a19ff2d788566",
+    "url": "/react-patient-manager/index.html"
+  },
+  {
+    "revision": "7311ed1b012afa743949",
+    "url": "/react-patient-manager/static/css/main.7b01c009.chunk.css"
+  },
+  {
+    "revision": "102bd3301aa3624e0cd3",
+    "url": "/react-patient-manager/static/js/2.f0218854.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-patient-manager/static/js/2.f0218854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7311ed1b012afa743949",
+    "url": "/react-patient-manager/static/js/main.0d79c669.chunk.js"
+  },
+  {
+    "revision": "7ca37682d5e03afc378b",
+    "url": "/react-patient-manager/static/js/runtime-main.e91b43f8.js"
+  }
+]);
